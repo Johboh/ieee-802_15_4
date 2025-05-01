d3724d5fc8785a7748cfcfc0773c0126d40ba118
Johan Böhlin <johboh@gmail.com>
2025-05-01 09:03:28 +0200

    Correct Serial.print()

--- examples/arduino/host/host.ino
+++ examples/arduino/host/host.ino
@@ -6,18 +6,20 @@ struct __attribute__((packed)) ApplicationMessage {
   double temperature;
 };
 
+char buffer[64];
+
 Ieee802154 _ieee802154({.handle_broadcasts = false}, [](Ieee802154::Message message) {
   Serial.println("Got Message");
-  Serial.println(" -- source MAC: 0x%llx", message.source_address);
-  Serial.println(" -- destination MAC: 0x%llx", message.destination_address);
-  Serial.println(" -- payload size: %d", message.payload_size);
+  sprintf(buffer, "source MAC: 0x%llx", message.source_address);
+  Serial.println(buffer);
+  sprintf(buffer, "destination MAC: 0x%llx", message.destination_address);
+  Serial.println(" -- payload size: " + String(message.payload_size));
   ApplicationMessage *app = reinterpret_cast<ApplicationMessage *>(message.payload);
   Serial.println(" -- Application.temperature: " + String(app->temperature));
 });
 
 void setup() {
   _ieee802154.initialize();
-  char buffer[64];
   sprintf(buffer, "This device IEEE802.15.4 MAC: 0x%llx", _ieee802154.deviceMacAddress());
   Serial.println(buffer);
 
